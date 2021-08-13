5b881b02a5ad4c5b1c02fb0cfc8c7fb3bfda5eff
Richard Xiong <rjx18@ic.ac.uk>
2021-08-13 10:03:48 +0100

    Add initial landing page

--- src/App.tsx
+++ src/App.tsx
@@ -8,16 +8,11 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          A permissionless, decentralised NFT ticketing platform, built on the THETA blockchain
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>
+          <b>Coming soon</b>
+        </p>
       </header>
     </div>
   );
